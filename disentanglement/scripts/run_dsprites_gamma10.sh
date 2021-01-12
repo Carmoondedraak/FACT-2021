b7b2a3152ce1fad2460296f60c4d441456612b57
André Jesus <lgpu0149@test3.irc.sara.nl.irc.sara.nl>
2021-01-12 12:07:10 +0100

    Separating into train and test scripts

--- disentanglement/scripts/run_dsprites_gamma10.sh
+++ disentanglement/scripts/run_dsprites_gamma10.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-python3 main.py --dataset dsprites --num_workers 4 --batch_size 64 \
+python3 train.py --dataset dsprites --num_workers 4 --batch_size 64 \
                --output_save True --viz_on True \
                --viz_ll_iter 100 --viz_la_iter 500 \
                --viz_ra_iter 1000 --viz_ta_iter 1000 \
