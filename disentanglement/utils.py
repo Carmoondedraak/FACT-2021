3b9c28fd48029c0156fd9c1ee337f57718ffc7d9
André Jesus <lgpu0149@test3.irc.sara.nl.irc.sara.nl>
2021-01-15 16:35:18 +0100

    Refactoring code vanila FactorVAE

--- disentanglement/utils.py
+++ disentanglement/utils.py
@@ -47,53 +47,317 @@ def mkdirs(path):
         os.makedirs(path)
 
 
-# Implementation of the disentanglement metrics
-import numpy
+# Definition of the FactorVAE base
+from abc import ABC, abstractmethod
 
-def factorvae_disentanglement_metric():
-    """
-        It is based on "Disentangling by Factorising" paper
-    """
-    scores_dict = {}
+import os
+import visdom
+from tqdm import tqdm
 
-    global_variances = compute_variances() #TODO
-    active_dims = prume_dimensions() #TODO
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision.utils import make_grid, save_image
+
+from ops import recon_loss, kl_divergence, permute_dims
+from model import FactorVAE1, FactorVAE2, Discriminator
+from dataset import return_data
+
+
+class BaseFactorVae(ABC):
+    def __init__(self):
+        # Misc
+        self.device = None
+        self.name = None
+        self.max_iter = None
+        self.print_iter = None
+        self.global_iter = None
+        self.pbar = None
+
+        # Data
+        self.dset_dir = None
+        self.dataset = None
+        self.batch_size = None
+        self.data_loader = None
+        self.data = None
+
+        # Networks & Optimizers
+        self.z_dim = None
+        self.gamma = None
+
+        self.lr_VAE = None
+        self.beta1_VAE = None
+        self.beta2_VAE = None
+
+        self.lr_D = None
+        self.beta1_D = None
+        self.beta2_D = None
+
+    def train(self):
+        raise NotImplementedError()
+
+    def generate_training_batch(self):
+        #TODO returns a numpy ndarray
+        raise NotImplementedError()
     
-    if not active_dims().any(): # TODO if there is [None]*dims ??
-        scores_dict[] = 0.
-        scores_dict[] = 0.
-        scores_dict[] = 0
-        return scores_dict
-    
-    training_votes = generate_training_batch() # TODO
-    classifier = np.argmax(training_votes, axis=0)
-    other_index = np.arange(training_votes.shape[1])
+    def visualize_recon(self):
+        data = self.image_gather.data
+        true_image = data['true'][0]
+        recon_image = data['recon'][0]
 
-    training_accuracy = np.sum(training_votes[classifier, other_index]) * 1. / np.sum(training_votes)
-    print("Training set accuracy: %.2g", training_accuracy)
+        true_image = make_grid(true_image)
+        recon_image = make_grid(recon_image)
+        sample = torch.stack([true_image, recon_image], dim=0)
+        self.viz.images(sample, env=self.name+'/recon_image',
+                        opts=dict(title=str(self.global_iter)))
 
-    eval_votes = generate_training_batch() # TODO
-    eval_accuracy = np.sum(eval_votes[classifier,
-                                    other_index]) * 1. / np.sum(eval_votes)
-    print("Evaluation set accuracy: %.2g", eval_accuracy)
+    def visualize_line(self):
+        data = self.line_gather.data
+        iters = torch.Tensor(data['iter'])
+        recon = torch.Tensor(data['recon'])
+        kld = torch.Tensor(data['kld'])
+        D_acc = torch.Tensor(data['acc'])
+        soft_D_z = torch.Tensor(data['soft_D_z'])
+        soft_D_z_pperm = torch.Tensor(data['soft_D_z_pperm'])
+        soft_D_zs = torch.stack([soft_D_z, soft_D_z_pperm], -1)
 
-    scores_dict["train_accuracy"] = train_accuracy
-    score_dict["eval_accuracy"] = eval_accuracy
-    scores_dict["num_active_dims"] = len(active_dims)
+        self.viz.line(X=iters,
+                      Y=soft_D_zs,
+                      env=self.name+'/lines',
+                      win=self.win_id['D_z'],
+                      update='append',
+                      opts=dict(
+                        xlabel='iteration',
+                        ylabel='D(.)',
+                        legend=['D(z)', 'D(z_perm)']))
+        self.viz.line(X=iters,
+                      Y=recon,
+                      env=self.name+'/lines',
+                      win=self.win_id['recon'],
+                      update='append',
+                      opts=dict(
+                        xlabel='iteration',
+                        ylabel='reconstruction loss',))
+        self.viz.line(X=iters,
+                      Y=D_acc,
+                      env=self.name+'/lines',
+                      win=self.win_id['acc'],
+                      update='append',
+                      opts=dict(
+                        xlabel='iteration',
+                        ylabel='discriminator accuracy',))
+        self.viz.line(X=iters,
+                      Y=kld,
+                      env=self.name+'/lines',
+                      win=self.win_id['kld'],
+                      update='append',
+                      opts=dict(
+                        xlabel='iteration',
+                        ylabel='kl divergence',))
 
-    return scores_dict
+    def visualize_traverse(self, limit=3, inter=2/3, loc=-1):
+        self.net_mode(train=False)
 
+        decoder = self.VAE.decode
+        encoder = self.VAE.encode
+        interpolation = torch.arange(-limit, limit+0.1, inter)
 
-def compute_variances():
-    #TODO
-    pass
+        random_img = self.data_loader.dataset.__getitem__(0)[1]
+        random_img = random_img.to(self.device).unsqueeze(0)
+        random_img_z = encoder(random_img)[:, :self.z_dim]
 
+        if self.dataset.lower() == 'dsprites':
+            fixed_idx1 = 87040 # square
+            fixed_idx2 = 332800 # ellipse
+            fixed_idx3 = 578560 # heart
 
-def prume_dimensions():
-    #TODO
-    pass
+            fixed_img1 = self.data_loader.dataset.__getitem__(fixed_idx1)[0]
+            fixed_img1 = fixed_img1.to(self.device).unsqueeze(0)
+            fixed_img_z1 = encoder(fixed_img1)[:, :self.z_dim]
 
+            fixed_img2 = self.data_loader.dataset.__getitem__(fixed_idx2)[0]
+            fixed_img2 = fixed_img2.to(self.device).unsqueeze(0)
+            fixed_img_z2 = encoder(fixed_img2)[:, :self.z_dim]
 
-def generate_training_batch():
-    #TODO returns a numpy ndarray
-    pass
+            fixed_img3 = self.data_loader.dataset.__getitem__(fixed_idx3)[0]
+            fixed_img3 = fixed_img3.to(self.device).unsqueeze(0)
+            fixed_img_z3 = encoder(fixed_img3)[:, :self.z_dim]
+
+            Z = {'fixed_square':fixed_img_z1, 'fixed_ellipse':fixed_img_z2,
+                 'fixed_heart':fixed_img_z3, 'random_img':random_img_z}
+
+        elif self.dataset.lower() == 'celeba':
+            fixed_idx1 = 191281 # 'CelebA/img_align_celeba/191282.jpg'
+            fixed_idx2 = 143307 # 'CelebA/img_align_celeba/143308.jpg'
+            fixed_idx3 = 101535 # 'CelebA/img_align_celeba/101536.jpg'
+            fixed_idx4 = 70059  # 'CelebA/img_align_celeba/070060.jpg'
+
+            fixed_img1 = self.data_loader.dataset.__getitem__(fixed_idx1)[0]
+            fixed_img1 = fixed_img1.to(self.device).unsqueeze(0)
+            fixed_img_z1 = encoder(fixed_img1)[:, :self.z_dim]
+
+            fixed_img2 = self.data_loader.dataset.__getitem__(fixed_idx2)[0]
+            fixed_img2 = fixed_img2.to(self.device).unsqueeze(0)
+            fixed_img_z2 = encoder(fixed_img2)[:, :self.z_dim]
+
+            fixed_img3 = self.data_loader.dataset.__getitem__(fixed_idx3)[0]
+            fixed_img3 = fixed_img3.to(self.device).unsqueeze(0)
+            fixed_img_z3 = encoder(fixed_img3)[:, :self.z_dim]
+
+            fixed_img4 = self.data_loader.dataset.__getitem__(fixed_idx4)[0]
+            fixed_img4 = fixed_img4.to(self.device).unsqueeze(0)
+            fixed_img_z4 = encoder(fixed_img4)[:, :self.z_dim]
+
+            Z = {'fixed_1':fixed_img_z1, 'fixed_2':fixed_img_z2,
+                 'fixed_3':fixed_img_z3, 'fixed_4':fixed_img_z4,
+                 'random':random_img_z}
+
+        elif self.dataset.lower() == '3dchairs':
+            fixed_idx1 = 40919 # 3DChairs/images/4682_image_052_p030_t232_r096.png
+            fixed_idx2 = 5172  # 3DChairs/images/14657_image_020_p020_t232_r096.png
+            fixed_idx3 = 22330 # 3DChairs/images/30099_image_052_p030_t232_r096.png
+
+            fixed_img1 = self.data_loader.dataset.__getitem__(fixed_idx1)[0]
+            fixed_img1 = fixed_img1.to(self.device).unsqueeze(0)
+            fixed_img_z1 = encoder(fixed_img1)[:, :self.z_dim]
+
+            fixed_img2 = self.data_loader.dataset.__getitem__(fixed_idx2)[0]
+            fixed_img2 = fixed_img2.to(self.device).unsqueeze(0)
+            fixed_img_z2 = encoder(fixed_img2)[:, :self.z_dim]
+
+            fixed_img3 = self.data_loader.dataset.__getitem__(fixed_idx3)[0]
+            fixed_img3 = fixed_img3.to(self.device).unsqueeze(0)
+            fixed_img_z3 = encoder(fixed_img3)[:, :self.z_dim]
+
+            Z = {'fixed_1':fixed_img_z1, 'fixed_2':fixed_img_z2,
+                 'fixed_3':fixed_img_z3, 'random':random_img_z}
+        else:
+            fixed_idx = 0
+            fixed_img = self.data_loader.dataset.__getitem__(fixed_idx)[0]
+            fixed_img = fixed_img.to(self.device).unsqueeze(0)
+            fixed_img_z = encoder(fixed_img)[:, :self.z_dim]
+
+            random_z = torch.rand(1, self.z_dim, 1, 1, device=self.device)
+
+            Z = {'fixed_img':fixed_img_z, 'random_img':random_img_z, 'random_z':random_z}
+
+        gifs = []
+        for key in Z:
+            z_ori = Z[key]
+            samples = []
+            for row in range(self.z_dim):
+                if loc != -1 and row != loc:
+                    continue
+                z = z_ori.clone()
+                for val in interpolation:
+                    z[:, row] = val
+                    sample = F.sigmoid(decoder(z)).data
+                    samples.append(sample)
+                    gifs.append(sample)
+            samples = torch.cat(samples, dim=0).cpu()
+            title = '{}_latent_traversal(iter:{})'.format(key, self.global_iter)
+            self.viz.images(samples, env=self.name+'/traverse',
+                            opts=dict(title=title), nrow=len(interpolation))
+
+        if self.output_save:
+            output_dir = os.path.join(self.output_dir, str(self.global_iter))
+            mkdirs(output_dir)
+            gifs = torch.cat(gifs)
+            gifs = gifs.view(len(Z), self.z_dim, len(interpolation), self.nc, 64, 64).transpose(1, 2)
+            for i, key in enumerate(Z.keys()):
+                for j, val in enumerate(interpolation):
+                    save_image(tensor=gifs[i][j].cpu(),
+                               filename=os.path.join(output_dir, '{}_{}.jpg'.format(key, j)),
+                               nrow=self.z_dim, pad_value=1)
+
+                grid2gif(str(os.path.join(output_dir, key+'*.jpg')),
+                         str(os.path.join(output_dir, key+'.gif')), delay=10)
+
+        self.net_mode(train=True)
+
+    def viz_init(self):
+        zero_init = torch.zeros([1])
+        self.viz.line(X=zero_init,
+                      Y=torch.stack([zero_init, zero_init], -1),
+                      env=self.name+'/lines',
+                      win=self.win_id['D_z'],
+                      opts=dict(
+                        xlabel='iteration',
+                        ylabel='D(.)',
+                        legend=['D(z)', 'D(z_perm)']))
+        self.viz.line(X=zero_init,
+                      Y=zero_init,
+                      env=self.name+'/lines',
+                      win=self.win_id['recon'],
+                      opts=dict(
+                        xlabel='iteration',
+                        ylabel='reconstruction loss',))
+        self.viz.line(X=zero_init,
+                      Y=zero_init,
+                      env=self.name+'/lines',
+                      win=self.win_id['acc'],
+                      opts=dict(
+                        xlabel='iteration',
+                        ylabel='discriminator accuracy',))
+        self.viz.line(X=zero_init,
+                      Y=zero_init,
+                      env=self.name+'/lines',
+                      win=self.win_id['kld'],
+                      opts=dict(
+                        xlabel='iteration',
+                        ylabel='kl divergence',))
+
+    def net_mode(self, train):
+        if not isinstance(train, bool):
+            raise ValueError('Only bool type is supported. True|False')
+
+        for net in self.nets:
+            if train:
+                net.train()
+            else:
+                net.eval()
+
+    def save_checkpoint(self, ckptname='last', verbose=True):
+        model_states = {'D':self.D.state_dict(),
+                        'VAE':self.VAE.state_dict()}
+        optim_states = {'optim_D':self.optim_D.state_dict(),
+                        'optim_VAE':self.optim_VAE.state_dict()}
+        states = {'iter':self.global_iter,
+                  'model_states':model_states,
+                  'optim_states':optim_states}
+
+        filepath = os.path.join(self.ckpt_dir, str(ckptname))
+        with open(filepath, 'wb+') as f:
+            torch.save(states, f)
+        if verbose:
+            self.pbar.write("=> saved checkpoint '{}' (iter {})".format(filepath, self.global_iter))
+
+    def load_checkpoint(self, ckptname='last', verbose=True):
+        if ckptname == 'last':
+            ckpts = os.listdir(self.ckpt_dir)
+            if not ckpts:
+                if verbose:
+                    self.pbar.write("=> no checkpoint found")
+                return
+
+            ckpts = [int(ckpt) for ckpt in ckpts]
+            ckpts.sort(reverse=True)
+            ckptname = str(ckpts[0])
+
+        filepath = os.path.join(self.ckpt_dir, ckptname)
+        if os.path.isfile(filepath):
+            with open(filepath, 'rb') as f:
+                checkpoint = torch.load(f)
+
+            self.global_iter = checkpoint['iter']
+            self.VAE.load_state_dict(checkpoint['model_states']['VAE'])
+            self.D.load_state_dict(checkpoint['model_states']['D'])
+            self.optim_VAE.load_state_dict(checkpoint['optim_states']['optim_VAE'])
+            self.optim_D.load_state_dict(checkpoint['optim_states']['optim_D'])
+            self.pbar.update(self.global_iter)
+            if verbose:
+                self.pbar.write("=> loaded checkpoint '{} (iter {})'".format(filepath, self.global_iter))
+        else:
+            if verbose:
+                self.pbar.write("=> no checkpoint found at '{}'".format(filepath))
+
---
+++ disentanglement/vanilla.py
@@ -0,0 +1,234 @@
+"""solver.py"""
+
+import argparse
+from utils import str2bool
+
+import os
+import visdom
+from tqdm import tqdm
+
+import numpy as np
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+
+from utils import DataGather, mkdirs, grid2gif, BaseFactorVae
+from ops import recon_loss, kl_divergence, permute_dims
+from model import FactorVAE1, FactorVAE2, Discriminator
+from dataset import return_data
+
+# To achieve reproducible results with sequential runs
+torch.backends.cudnn.enabled = True
+torch.backends.cudnn.benchmark = True
+
+init_seed = 1
+torch.manual_seed(init_seed)
+torch.cuda.manual_seed(init_seed)
+np.random.seed(init_seed)
+
+
+class Solver(BaseFactorVae):
+    def __init__(self, args):
+        # Misc
+        use_cuda = args.cuda and torch.cuda.is_available()
+        self.device = 'cuda' if use_cuda else 'cpu'
+        self.name = args.name
+        self.max_iter = int(args.max_iter)
+        self.print_iter = args.print_iter
+        self.global_iter = 0
+        self.pbar = tqdm(total=self.max_iter)
+
+        # Data
+        self.dset_dir = args.dset_dir
+        self.dataset = args.dataset
+        self.batch_size = args.batch_size
+        self.data_loader, self.data = return_data(args)
+
+        # Networks & Optimizers
+        self.z_dim = args.z_dim
+        self.gamma = args.gamma
+
+        self.lr_VAE = args.lr_VAE
+        self.beta1_VAE = args.beta1_VAE
+        self.beta2_VAE = args.beta2_VAE
+
+        self.lr_D = args.lr_D
+        self.beta1_D = args.beta1_D
+        self.beta2_D = args.beta2_D
+
+        if args.dataset == 'dsprites':
+            self.VAE = FactorVAE1(self.z_dim).to(self.device)
+            self.nc = 1
+        else:
+            self.VAE = FactorVAE2(self.z_dim).to(self.device)
+            self.nc = 3
+        self.optim_VAE = optim.Adam(self.VAE.parameters(), lr=self.lr_VAE,
+                                    betas=(self.beta1_VAE, self.beta2_VAE))
+
+        self.D = Discriminator(self.z_dim).to(self.device)
+        self.optim_D = optim.Adam(self.D.parameters(), lr=self.lr_D,
+                                  betas=(self.beta1_D, self.beta2_D))
+
+        self.nets = [self.VAE, self.D]
+
+        # Visdom
+        self.viz_on = args.viz_on
+        self.win_id = dict(D_z='win_D_z', recon='win_recon', kld='win_kld', acc='win_acc')
+        self.line_gather = DataGather('iter', 'soft_D_z', 'soft_D_z_pperm', 'recon', 'kld', 'acc')
+        self.image_gather = DataGather('true', 'recon')
+        if self.viz_on:
+            self.viz_port = args.viz_port
+            self.viz = visdom.Visdom(log_to_filename='./logging.log', offline=True)  # server='http://188.140.46.6' ,port=self.viz_port)
+            self.viz_ll_iter = args.viz_ll_iter
+            self.viz_la_iter = args.viz_la_iter
+            self.viz_ra_iter = args.viz_ra_iter
+            self.viz_ta_iter = args.viz_ta_iter
+            if not self.viz.win_exists(env=self.name+'/lines', win=self.win_id['D_z']):
+                self.viz_init()
+
+        # Checkpoint
+        self.ckpt_dir = os.path.join(args.ckpt_dir, args.name)
+        self.ckpt_save_iter = args.ckpt_save_iter
+        mkdirs(self.ckpt_dir)
+        if args.ckpt_load:
+            self.load_checkpoint(args.ckpt_load)
+
+        # Output(latent traverse GIF)
+        self.output_dir = os.path.join(args.output_dir, args.name)
+        self.output_save = args.output_save
+        mkdirs(self.output_dir)
+
+    def train(self):
+        self.net_mode(train=True)
+
+        ones = torch.ones(self.batch_size, dtype=torch.long, device=self.device)
+        zeros = torch.zeros(self.batch_size, dtype=torch.long, device=self.device)
+
+        out = False
+        while not out:
+            for x_true1, x_true2 in self.data_loader:
+                self.global_iter += 1
+                self.pbar.update(1)
+
+                x_true1 = x_true1.to(self.device)
+                x_recon, mu, logvar, z = self.VAE(x_true1)
+                vae_recon_loss = recon_loss(x_true1, x_recon)
+                vae_kld = kl_divergence(mu, logvar)
+
+                D_z = self.D(z)
+                vae_tc_loss = (D_z[:, :1] - D_z[:, 1:]).mean()
+
+                vae_loss = vae_recon_loss + vae_kld + self.gamma*vae_tc_loss
+
+                self.optim_VAE.zero_grad()
+                vae_loss.backward(retain_graph=True)
+                self.optim_VAE.step()
+
+                x_true2 = x_true2.to(self.device)
+                z_prime = self.VAE(x_true2, no_dec=True)
+                z_pperm = permute_dims(z_prime).detach()
+                D_z_pperm = self.D(z_pperm)
+                D_tc_loss = 0.5*(F.cross_entropy(D_z, zeros) + F.cross_entropy(D_z_pperm, ones))
+
+                self.optim_D.zero_grad()
+                D_tc_loss.backward()
+                self.optim_D.step()
+
+                if self.global_iter%self.print_iter == 0:
+                    self.pbar.write('[{}] vae_recon_loss:{:.3f} vae_kld:{:.3f} vae_tc_loss:{:.3f} D_tc_loss:{:.3f}'.format(
+                        self.global_iter, vae_recon_loss.item(), vae_kld.item(), vae_tc_loss.item(), D_tc_loss.item()))
+
+                if self.global_iter%self.ckpt_save_iter == 0:
+                    self.save_checkpoint(self.global_iter)
+
+                if self.viz_on and (self.global_iter%self.viz_ll_iter == 0):
+                    soft_D_z = F.softmax(D_z, 1)[:, :1].detach()
+                    soft_D_z_pperm = F.softmax(D_z_pperm, 1)[:, :1].detach()
+                    D_acc = ((soft_D_z >= 0.5).sum() + (soft_D_z_pperm < 0.5).sum()).float()
+                    D_acc /= 2*self.batch_size
+                    self.line_gather.insert(iter=self.global_iter,
+                                            soft_D_z=soft_D_z.mean().item(),
+                                            soft_D_z_pperm=soft_D_z_pperm.mean().item(),
+                                            recon=vae_recon_loss.item(),
+                                            kld=vae_kld.item(),
+                                            acc=D_acc.item())
+
+                if self.viz_on and (self.global_iter%self.viz_la_iter == 0):
+                    self.visualize_line()
+                    self.line_gather.flush()
+
+                if self.viz_on and (self.global_iter%self.viz_ra_iter == 0):
+                    self.image_gather.insert(true=x_true1.data.cpu(),
+                                             recon=F.sigmoid(x_recon).data.cpu())
+                    self.visualize_recon()
+                    self.image_gather.flush()
+
+                if self.viz_on and (self.global_iter%self.viz_ta_iter == 0):
+                    if self.dataset.lower() == '3dchairs':
+                        self.visualize_traverse(limit=2, inter=0.5)
+                    else:
+                        self.visualize_traverse(limit=3, inter=2/3)
+
+                if self.global_iter >= self.max_iter:
+                    out = True
+                    break
+
+        self.pbar.write("[Training Finished]")
+        self.pbar.close()
+
+    def visualize_recon(self):
+        super().visualize_recon()
+
+    def visualize_line(self):
+        super().visualize_line()
+
+    def visualize_traverse(self, limit=3, inter=2/3, loc=-1):
+        super().visualize_traverse(limit, inter, loc)
+
+    def viz_init(self):
+        super().viz_init()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Factor-VAE')
+
+    parser.add_argument('--name', default='main', type=str, help='name of the experiment')
+    parser.add_argument('--cuda', default=True, type=str2bool, help='enable cuda')
+    parser.add_argument('--max_iter', default=1e6, type=float, help='maximum training iteration')
+    parser.add_argument('--batch_size', default=64, type=int, help='batch size')
+
+    parser.add_argument('--z_dim', default=10, type=int, help='dimension of the representation z')
+    parser.add_argument('--gamma', default=6.4, type=float, help='gamma hyperparameter')
+    parser.add_argument('--lr_VAE', default=1e-4, type=float, help='learning rate of the VAE')
+    parser.add_argument('--beta1_VAE', default=0.9, type=float, help='beta1 parameter of the Adam optimizer for the VAE')
+    parser.add_argument('--beta2_VAE', default=0.999, type=float, help='beta2 parameter of the Adam optimizer for the VAE')
+    parser.add_argument('--lr_D', default=1e-4, type=float, help='learning rate of the discriminator')
+    parser.add_argument('--beta1_D', default=0.5, type=float, help='beta1 parameter of the Adam optimizer for the discriminator')
+    parser.add_argument('--beta2_D', default=0.9, type=float, help='beta2 parameter of the Adam optimizer for the discriminator')
+
+    parser.add_argument('--dset_dir', default='data', type=str, help='dataset directory')
+    parser.add_argument('--dataset', default='CelebA', type=str, help='dataset name')
+    parser.add_argument('--image_size', default=64, type=int, help='image size. now only (64,64) is supported')
+    parser.add_argument('--num_workers', default=2, type=int, help='dataloader num_workers')
+
+    parser.add_argument('--viz_on', default=True, type=str2bool, help='enable visdom visualization')
+    parser.add_argument('--viz_port', default=8097, type=int, help='visdom port number')
+    parser.add_argument('--viz_ll_iter', default=1000, type=int, help='visdom line data logging iter')
+    parser.add_argument('--viz_la_iter', default=5000, type=int, help='visdom line data applying iter')
+    parser.add_argument('--viz_ra_iter', default=10000, type=int, help='visdom recon image applying iter')
+    parser.add_argument('--viz_ta_iter', default=10000, type=int, help='visdom traverse applying iter')
+
+    parser.add_argument('--print_iter', default=500, type=int, help='print losses iter')
+
+    parser.add_argument('--ckpt_dir', default='checkpoints', type=str, help='checkpoint directory')
+    parser.add_argument('--ckpt_load', default=None, type=str, help='checkpoint name to load')
+    parser.add_argument('--ckpt_save_iter', default=10000, type=int, help='checkpoint save iter')
+
+    parser.add_argument('--output_dir', default='outputs', type=str, help='output directory')
+    parser.add_argument('--output_save', default=True, type=str2bool, help='whether to save traverse results')
+
+    args = parser.parse_args()
+
+    solver = Solver(args)
+    solver.train()
+    #solver.disentanglement_metric()
