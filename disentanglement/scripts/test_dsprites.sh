7db12a8a54f04e56165673ff71cccb729ea5454f
André Jesus <lgpu0149@test3.irc.sara.nl.irc.sara.nl>
2021-01-23 20:59:50 +0100

    Updating scripts

--- disentanglement/scripts/test_dsprites.sh
+++ disentanglement/scripts/test_dsprites.sh
@@ -5,5 +5,5 @@ python3 test_factorvae.py --dataset dsprites --num_workers 4 --batch_size 64 \
                --ckpt_save_iter 1000 --max_iter 6e3 \
                --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 \
                --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
-	       --lambdaa 1.0 --ckpt_dir experi_attention_factorvae \
+	       --lambdaa 1.0 --ckpt_dir experi_attention_factorvae_3000_verify \
                --name test/ --z_dim 10 --gamma 10 --ckpt_load last
