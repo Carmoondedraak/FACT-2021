8c0bc07a253bb5bf147033598b1b6717afeea385
KJW <kevinwaller@ziggo.nl>
2021-01-25 23:57:29 +0100

    Added OneClassMNIST for RBM to train on

---
+++ rbm_anomaly_maps/OneClassMNIST.py
@@ -0,0 +1,19 @@
+
+import torch
+from torchvision.datasets import MNIST
+
+# Extends torchvision MNISt dataset, overwriting the data to only have a single class
+class OneClassMNIST(MNIST):
+    def __init__(self, digit, root, transforms, download=False, train=True):
+        super(OneClassMNIST, self).__init__(root, train=train, download=download)
+
+        # Get indices of images of specified digit
+        self.digit = digit
+        digit_idxs = torch.nonzero(self.targets == digit).squeeze(1)
+
+        # Overwrite data and targets to only contain the images of the specified digit
+        self.data = self.data[digit_idxs, :,:]
+        self.targets = torch.full(digit_idxs.shape, fill_value=digit)
+
+        # Overwrite the transforms to be used
+        self.transform = transforms
